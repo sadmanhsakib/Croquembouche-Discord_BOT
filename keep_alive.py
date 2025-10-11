b8e407c822d4f3e59453dab6130de648ef1d9766
Sadman H. Sakib <831.sakib@gmail.com>
2025-10-11 18:41:48 +0600

    Changed the port in keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Active."
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
 
 def keep_alive():
     t = Thread(target=run)
